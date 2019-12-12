ad69bda127ee6e5502c55c75bbb38b09b6a37f87
yasnitsky <frenk.b26@gmail.com>
2019-12-12 13:42:23 +0300

    Implements Field class

---
+++ src/io/hexlet/model/Field.java
@@ -0,0 +1,24 @@
+package io.hexlet.model;
+
+public class Field {
+
+    private static int FIELD_SIZE = 3;
+
+    private static Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getFieldSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure[][] getField() {
+        return field;
+    }
+
+    public Figure getFigure(Point point){
+        return field[point.getX()][point.getY()];
+    }
+
+    public void setFigure(Point point, Figure figure){
+        field[point.getX()][point.getY()] = figure;
+    }
+}
