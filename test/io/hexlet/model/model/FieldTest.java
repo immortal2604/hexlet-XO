bcd11b82118e2ddcd9868e54a476a20ebdce9715
Yasnitskiy <frenk.b26@gmail.com>
2019-12-15 19:25:27 +0300

    implements FieldTest

---
+++ test/io/hexlet/model/model/FieldTest.java
@@ -0,0 +1,24 @@
+package io.hexlet.model.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    @Test
+    public void getFieldSize() {
+    }
+
+    @Test
+    public void getField() {
+    }
+
+    @Test
+    public void getFigure() {
+    }
+
+    @Test
+    public void setFigure() {
+    }
+}
