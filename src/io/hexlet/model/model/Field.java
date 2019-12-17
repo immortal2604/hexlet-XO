9b3016a57a8984d120c5d66272e49e8cf64dbf7f
yasnitsky <frenk.b26@gmail.com>
2019-12-17 14:06:43 +0300

    some Refactoring

--- src/io/hexlet/model/model/Field.java
+++ src/io/hexlet/model/model/Field.java
@@ -7,18 +7,23 @@ public class Field {
     private static Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getFieldSize() {
+
         return FIELD_SIZE;
     }
 
     public Figure[][] getField() {
+
         return field;
     }
 
     public Figure getFigure(Point point){
+
         return field[point.getX()][point.getY()];
     }
 
     public void setFigure(Point point, Figure figure){
+
         field[point.getX()][point.getY()] = figure;
     }
 }
+
