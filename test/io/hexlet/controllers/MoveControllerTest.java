c1ced33cc388ea4f54353a2d7571bfa727f3d5de
Yasnitskiy <frenk.b26@gmail.com>
2019-12-20 00:08:15 +0300

    implements movecontroller testing for 4 opportunities

--- test/io/hexlet/controllers/MoveControllerTest.java
+++ test/io/hexlet/controllers/MoveControllerTest.java
@@ -1,5 +1,7 @@
 package io.hexlet.controllers;
 
+import io.hexlet.exeptions.AlreadyOccupiedExeption;
+import io.hexlet.exeptions.InvalidCoordinatesExeptions;
 import io.hexlet.model.Field;
 import io.hexlet.model.Figure;
 import io.hexlet.model.Point;
@@ -9,13 +11,63 @@ import static org.junit.Assert.*;
 
 public class MoveControllerTest {
 
-    final Field field = new Field();
-    final Figure figure = Figure.X;
-    final Point point = new Point(4,2);
+    @Test
+    public void applyFigurePerfectSituation() {
+        final Field field = new Field();
+        final Figure figure = Figure.X;
+        final Point point = new Point(0,2);
+        final MoveController moveController = new MoveController();
+        try {
+            moveController.applyFigure(field, figure, point);
+        } catch (InvalidCoordinatesExeptions e) {
+            e.printStackTrace();
+        } catch (AlreadyOccupiedExeption e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
-    public void applyFigure() {
+    public void applyFigureOccupedField() {
+        final Field field = new Field();
+        final Figure figure = Figure.X;
+        final Point point = new Point(0,2);
+        field.setFigure(point,figure);
 
+        final MoveController moveController = new MoveController();
+        try {
+            moveController.applyFigure(field, figure, point);
+        } catch (InvalidCoordinatesExeptions e) {
+            e.printStackTrace();
+        } catch (AlreadyOccupiedExeption e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void applyFigureInvalidXcoordinate() {
+        final Field field = new Field();
+        final Figure figure = Figure.X;
+        final Point point = new Point(3,2);
+
+        final MoveController moveController = new MoveController();
+        try {
+            moveController.applyFigure(field, figure, point);
+        } catch (InvalidCoordinatesExeptions e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void applyFigureInvalidYcoordinate() {
+        final Field field = new Field();
+        final Figure figure = Figure.X;
+        final Point point = new Point(2,3);
+
+        final MoveController moveController = new MoveController();
+        try {
+            moveController.applyFigure(field, figure, point);
+        } catch (InvalidCoordinatesExeptions e) {
+            e.printStackTrace();
+        }
     }
 }
